e8fc16273dfca7fa5c0d71fbb6e19c132a5c85cd
Zhihan Fang <zhihan.fang@rutgers.edu>
2015-11-09 22:14:59 -0500

    import tsv success

---
+++ java/src/io/github/coding/test/BaseConnectionTest.java
@@ -0,0 +1,11 @@
+package io.github.coding.test;
+
+import io.github.dao.BaseConnection;
+
+public class BaseConnectionTest {
+	public static void main(String args[]){
+		BaseConnection bc = new BaseConnection();
+		
+	}
+}
+
